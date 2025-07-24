9c5bc02fdcc3baa8a216b3bc7f6e3434c43065e2
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-07-24 11:04:56 +0545

    Some modify Profile Update

--- modules/chef.py
+++ modules/chef.py
@@ -4,11 +4,11 @@ from modules.crud import update_profile
 FILE_NAME = 'data/users.txt'
 MENU_FILE = 'data/menu.txt'
 
-def chef_interface(username):
-    print(f"{username.capitalize()}, Welcome to the Restaurant Management System!")
-    chef_menu()
+def update_chef_profile(username):
+    update_profile(username)
+    chef_menu(username)
 
-def chef_menu():
+def chef_menu(username):
     print("\n" + "═" * 50)
     print("Chef Menu".center(50))
     print("═" * 50)
@@ -17,9 +17,14 @@ def chef_menu():
     print("3. Update Profile")
     choose_number = int(input("Enter your choice: "))
     match choose_number:
-        case "1":
+        case 1:
             print("view Orders")
-        case "2":
+        case 2:
             print("Update Orders Status")
-        case "3":
-            update_profile(None)
+        case 3:
+            update_chef_profile(username)
+
+
+def chef_interface(username):
+    print(f"{username.capitalize()}, Welcome to the Restaurant Management System!")
+    chef_menu(username)
