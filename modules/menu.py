e96c373cb4ac9e699e5b92c91577891c0c07ea78
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-07-28 00:57:02 +0545

    Adding View Menu Feature

--- modules/menu.py
+++ modules/menu.py
@@ -1,29 +1,29 @@
 import os
 
 MENU_FILE = 'data/menu.txt'
+ORDERS_FILE = 'data/orders.txt'
 
-def view_order_menu():
-    print("\n" + "═" * 50)
-    print("View Menu".upper().center(50))
-    print("═" * 50)
+def view_menu():
+    from modules.customer import customer_menu
+    print("\n" + "═" * 60)
+    print("View Menu".upper().center(60))
+    print("═" * 60)
     food_id = 1
     file = open(MENU_FILE, 'r')
+    print(f"{'S.N':<5}{'Name':<20}{'Category':<15}{'Unit':<10}{'Price'}")
+    print("-" * 60)
     for line in file:
-        stored_name, stored_price = line.strip().split(",")
-        print(f"{food_id}. Food Name: {stored_name}: Price: {stored_price}")
+        name, category, unit, price = line.strip().split(",")
+        print(f"{food_id:<5}{name:<20}{category:<15}{unit:<10}{price}")
         food_id += 1
-    print("\n" + "═" * 50)
-    user_choose = input("If you would like to place order, enter Y or N: ")
-    match user_choose.lower():
-        case "y":
-            place_order()
-        case "n":
-            exit()
-
-def place_order():
-    print("Place Order Interface".center(50))
-    print("═" * 50)
-    # choose_item = int(input("Enter food item number: "))
+    print("\n" + "═" * 60)
+    # user_choose = input("If you would like to place order, enter Y or N: ")
+    # match user_choose.lower():
+    #     case "y":
+    #         place_order()
+    #     case "n":
+    #         exit()
+    customer_menu(None)
 
 def pay_order():
     print("\n" + "═" * 50)
