0bb4069aedf84d515310b2ff852535839208a31e
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-07-29 16:05:42 +0545

    Adding a view sales reports

--- modules/admin.py
+++ modules/admin.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
 
 from modules.crud import admin_add_user, view_staff, update_profile, update_user_profile, delete_user_profile
+from modules.menu import ORDERS_FILE
 
 USER_FILE = 'data/users.txt'
 FEEDBACK_FILE = 'data/feedback.txt'
@@ -45,8 +47,65 @@ def manage_staff(username):
             print("-" * 50)
 
 
-def view_sales_report():
-    print("View Sales Report")
+def view_sales_report(username):
+    print("\n" + "═" * 50)
+    print("SALES REPORT PANEL".center(50))
+    print("═" * 50)
+
+    if not os.path.exists(ORDERS_FILE):
+        print("\n" + "-" * 50)
+        print("No order data found.")
+        print("-" * 50)
+        return
+
+    with open(ORDERS_FILE, "r") as file:
+        lines = file.readlines()
+
+    # Only process valid completed & paid orders
+    valid_orders = []
+    for line in lines:
+        parts = line.strip().split(",")
+        if len(parts) == 6:
+            user, item, qty, price, order_status, payment_status = parts
+            if order_status.lower() == "complete" and payment_status.lower() == "paid":
+                valid_orders.append({
+                    "user": user,
+                    "item": item,
+                    "qty": int(qty),
+                    "price": float(price)
+                })
+
+    if not valid_orders:
+        print("\n" + "-" * 50)
+        print("No completed and paid orders available.")
+        print("-" * 50)
+        admin_interface(username)
+        return
+
+    # Menu to choose report type
+    print("1. Total Sales")
+    try:
+        choice = int(input("Choose an option: "))
+    except ValueError:
+        print("/n" + "-" * 50)
+        print("Invalid input.".center(50))
+        print("-" * 50)
+        admin_interface(username)
+        return
+    print("\n" + "-" * 50)
+
+    if choice == 1:
+        total_sales = sum(order["price"] for order in valid_orders)
+        print(f"TOTAL SALES AMOUNT: Rs.{total_sales:.2f}")
+        print("-" * 50)
+        admin_interface(username)
+    else:
+        print("Invalid option selected.".center(50))
+        print("-" * 50)
+        admin_interface(username)
+
+    print("-" * 50)
+
 
 def view_feedback(stored_username):
     print("\n" + "═" * 50)
@@ -105,7 +164,7 @@ def admin_interface(username):
                     manage_staff(username)
                     break
                 case 2:
-                    view_sales_report()
+                    view_sales_report(username)
                     break
                 case 3:
                     view_feedback(username)
